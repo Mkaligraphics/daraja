530b1a9770b7003af336359eacccbfe1100bd586
Mkaligraphics <nkhalammar@gmail.com>
2025-09-10 08:16:34 +0300

    stk push

---
+++ callbackinfo.php
@@ -0,0 +1,9 @@
+{"Body":
+    {"stkCallback":
+        {"MerchantRequestID":"e6d0-4ee5-b894-2bf12f323bfa14681","CheckoutRequestID":"ws_CO_100920250804340704487610","ResultCode":2001,"ResultDesc":"The initiator information is invalid."}
+    }
+}
+        
+{"Body":
+    {"stkCallback":
+        {"MerchantRequestID":"b8ae-4f7c-b56b-875ff450c56c14703","CheckoutRequestID":"ws_CO_100920250807586704487610","ResultCode":0,"ResultDesc":"The service request is processed successfully.","CallbackMetadata":{"Item":[{"Name":"Amount","Value":1.00},{"Name":"MpesaReceiptNumber","Value":"TIA980V39D"},{"Name":"Balance","Value":0.00},{"Name":"TransactionDate","Value":20250910080814},{"Name":"PhoneNumber","Value":254704487610}]}}}}
